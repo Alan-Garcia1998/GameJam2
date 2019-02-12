09cc4970c91fbe5400c8b8be4d71d703fbde8467
Kaz <cas@Cass-MacBook-Pro.local>
2019-02-12 02:58:13 -0700

    YAW YEEEEET

---
+++ GameJam2/Assets/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public double time;
+    private int frame = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.0f;
+        timerText.text = "Score: " + time.ToString();
+        int frame = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(frame == 60){
+            time++;
+            frame = 0;
+        }
+        
+        timerText.text = "Time Elapsed: " + time.ToString();
+        frame++;
+    }
+
+}
