b3e740e3a4c58db3ed89607b4d24c933b0e823a2
Kaz <cas@Cass-MacBook-Pro.local>
2019-02-11 23:36:05 -0700

    Yaw

---
+++ GameJam2/Assets/gun.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    public Camera fpsCam;
+    public float num = 200f;//public bool fLoat = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<ConstantForce>().relativeForce = new Vector3(0, -10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            shoot();
+
+           
+        }
+    }
+
+
+    void shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+            num = num * -1;
+            GetComponent<ConstantForce>().relativeForce = new Vector3(0, num, 0);
+        }
+    }
+}
